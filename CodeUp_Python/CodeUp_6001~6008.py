70038d8424031b105983814e25efb8b6b70f4b73
채현우 <62530720+chaehyuenwoo@users.noreply.github.com>
2022-07-14 00:00:50 +0900

    [기초-출력] 6001~6008번.py

---
+++ CodeUp_Python/CodeUp_6001~6008.py
@@ -0,0 +1,27 @@
+# [기초-출력]
+
+
+#6001
+print('Hello')
+
+#6002
+print('Hello World')
+
+#6003
+print('Hello')
+print('World')
+
+#6004
+print("'Hello'")
+
+#6005
+print('"Hello World"')
+
+#6006
+print("\"!@#$%^&*()\'")
+
+#6007
+print("\"C:\\Download\\'hello'.py\"")
+
+#6008
+print("print(\"Hello\\nWorld\")")
